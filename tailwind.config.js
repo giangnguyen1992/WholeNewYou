ca8b31601bbe0e83b962c68c7cd0e40c4cf69d9a
Giang Nguyen <giang.nguyen@spiegel.de>
2020-05-23 09:54:55 +0200

    added initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+module.exports = {
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            black: '#000000',
+            white: '#FFFFFF',
+            'primary-base': '#ffab9f',
+            'primary-light': '#f6d0c4',
+            'primary-lighter': '#f6e8e7',
+            'sec-base': '#425966',
+            'sec-light': '#9db5c4',
+            'sec2-base': '#fddf9a',
+            'sec2-light': '#f8edd3'
+        },
+        fontFamily: {
+            sans: ['Source Sans Pro', 'sans-serif'],
+            heading: ['Bebas Neue', 'sans-serif']
+        },
+        screens: {
+            sm: { max: '719px' },
+            md: { min: '720px', max: '1011px' },
+            lg: { min: '1012px' }
+        },
+        textColor: (theme) => theme('colors'),
+        backgroundColor: (theme) => theme('colors')
+    },
+    variants: {},
+    plugins: []
+};
