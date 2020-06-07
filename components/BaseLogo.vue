f78eb7addc2541c144a881b5ae7332e626e04062
Giang Nguyen <giang.nguyen@spiegel.de>
2020-06-07 22:19:49 +0200

    changed early bird price and added socialbar to the about me section

--- components/BaseLogo.vue
+++ components/BaseLogo.vue
@@ -1,5 +1,5 @@
 <template>
-    <figure class="mx-auto w-48">
+    <figure class="mx-auto lg:w-56 md:w-56 sm:w-48">
         <img src="~/assets/images/logo.svg" alt="Logo" class="mx-auto" />
     </figure>
 </template>
